709e3e605499f41c90a1366800b65d54f1d576a6
Quang <nmquang2012@gmail.com>
2021-03-07 13:58:24 +0700

    Add main header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="App">
+      <h1 className="h1-todos">todos</h1>
       <TodoList />
     </div>
   );

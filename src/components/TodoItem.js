1bc970475d747844ccd9750d3b773d362766f6a3
Quang <nmquang2012@gmail.com>
2021-03-11 22:26:26 +0700

    Clean code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,21 +3,29 @@ import classNames from 'classnames';
 
 import './CSS/Components.css';
 
-import circle from '../img/circle.svg';
 import checkCompleted from '../img/check-completed.svg';
 import closeIcon from '../img/close.svg';
 
 export default function TodoItem(props) {
     const { item, onClickIcon, onClickDestroy } = props;
-    const src = item.completed ? checkCompleted : circle;
 
     return(
         <div className={classNames("todo-item", {
             'todo-item-completed': item.completed
         })}>
-            <img className="icon" onClick={onClickIcon} src={src} alt='' />
+            <img 
+                className="icon" 
+                onClick={onClickIcon} 
+                src={checkCompleted} 
+                alt='' 
+            />
             <p>{item.title}</p>
-            <img className='close-icon' onClick={onClickDestroy} src={closeIcon} alt='' />
+            <img 
+                className='close-icon' 
+                onClick={onClickDestroy} 
+                src={closeIcon} 
+                alt='' 
+            />
         </div>
     );
 }
